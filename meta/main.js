5321eb4449611b16a6084d213b3cf7ae543d1059
Eri-gon2 <ergan@ucsd.edu>
2025-05-31 11:17:08 -0700

    lab8

--- meta/main.js
+++ meta/main.js
@@ -374,7 +374,6 @@ function updateTimeDisplay() {
 }
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 function updateFileDisplay() {
-  console.log('filteredCommits:', filteredCommits);
   let lines = filteredCommits.flatMap((d) => d.lines);
   let files = d3
   .groups(lines, (d) => d.file)
@@ -433,8 +432,38 @@ d3.select('#scatter-story')
 	`,
   );
 
+  d3.select('#scatter-story2')
+  .selectAll('.step')
+  .data(commits)
+  .join('div')
+  .attr('class', 'step')
+  .html(
+    (d, i) => `
+		On ${d.datetime.toLocaleString('en', {
+      dateStyle: 'full',
+      timeStyle: 'short',
+    })},
+		I made <a href="${d.url}" target="_blank">${
+      i > 0 ? 'another glorious commit' : 'my first commit, and it was glorious'
+    }</a>.
+		I edited ${d.totalLines} lines across ${
+      d3.rollups(
+        d.lines,
+        (D) => D.length,
+        (d) => d.file,
+      ).length
+    } files.
+		Then I looked over all I had made, and I saw that it was very good.
+	`,
+  );
+
 function onStepEnter(response) {
   console.log(response.element.__data__.datetime);
+  commitMaxTime = response.element.__data__.datetime;
+  filterCommitsByTime(); // filters by time and assign to some top-level variable.
+  updateScatterPlot(data, filteredCommits);
+  renderCommitInfo(data, filteredCommits);
+  updateFileDisplay();
 }
 
 const scroller = scrollama();
